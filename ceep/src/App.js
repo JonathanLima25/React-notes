b5842ed8705dfec713369d67503200f963e636b0
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-12 16:15:58 -0300

    Utiliza state e props para passar atributos entre os componentes

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -5,15 +5,26 @@ import "./assets/App.css";
 import "./assets/index.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notas:[]
+    };
+  }
   criarNota(titulo, texto){
-    console.log(`uma nova nota criada!\n` + titulo +`\n` + texto);
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoState = {
+      notas: novoArrayNotas
+    }
+    this.setState(novoState)
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota} />
-        <ListaDeNotas/>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
