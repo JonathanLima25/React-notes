bbb5ceecfb7b6148acf9c9008bfac495cc5a3ab5
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-14 20:02:18 -0300

    funcionalidade separada da renderizacao

--- ceep/src/dados/Notas.js
+++ ceep/src/dados/Notas.js
@@ -2,6 +2,7 @@ export default class ArrayDeNotas{
     constructor(){
         this.notas = [];
         this._inscritos = [];
+
     }
 
     inscrever(func){
@@ -9,16 +10,19 @@ export default class ArrayDeNotas{
     }
 
     notificar(){
-        this._inscritos.forEach(func => func(this.categorias))
+        this._inscritos.forEach(func => func(this.notas))
     }
 
     adicionarNota(titulo, texto, categoria){
         const novaNota = new Nota(titulo, texto, categoria);
         this.notas.push(novaNota);
+        this.notificar();
     }
 
     apagarNota(indice){
         this.notas.splice(indice,1);
+        this.notificar();
+
     }
 }
 
