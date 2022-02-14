4e8cde5ec47acbb268a4802a277b65ea97f66aaf
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-02-14 15:00:06 -0300

    substitui o state

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -4,49 +4,55 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import ListaCategorias from "./components/ListaCategorias";
 import "./assets/App.css";
 import "./assets/index.css";
+import Categorias from "./dados/Categorias";
+import ArrayDeNotas from "./dados/Notas";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      notas:[],
-      categorias:[]
-    };
-  }
-  criarNota(titulo, texto, categoria){
-    const novaNota = {titulo, texto, categoria};
-    const novoArrayNotas = [...this.state.notas,novaNota]
-    const novoState = {
-      notas: novoArrayNotas
-    }
-    this.setState(novoState)
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas()
+    // this.state = {
+    //   notas:[],
+    //   // categorias:[]
+    // };
   }
 
-  adicionarCategoria(nomeCategoria){
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
-    const novostate = {...this.state, categorias:novoArrayCategorias};
-    this.setState(novostate);
-  }
 
-  deletarNota(index){
-    let arrayNotas = this.state.notas;
-    arrayNotas.splice(index,1);
-    this.setState({notas:arrayNotas});
-  }
+  // criarNota(titulo, texto, categoria){
+  //   const novaNota = {titulo, texto, categoria};
+  //   const novoArrayNotas = [...this.state.notas,novaNota]
+  //   const novoState = {
+  //     notas: novoArrayNotas
+  //   }
+  //   this.setState(novoState)
+  // }
+
+  // adicionarCategoria(nomeCategoria){
+  //   const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
+  //   const novostate = {...this.state, categorias:novoArrayCategorias};
+  //   this.setState(novostate);
+  // }
+
+  // deletarNota(index){
+  //   let arrayNotas = this.state.notas;
+  //   arrayNotas.splice(index,1);
+  //   this.setState({notas:arrayNotas});
+  // }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro categorias={this.state.categorias} 
-        criarNota={this.criarNota.bind(this)} />
+        <FormularioCadastro categorias={this.categorias.categorias} 
+        criarNota={this.notas.criarNota} />
         <main className="conteudo-principal">
           <ListaCategorias 
-          adicionarCategoria={this.adicionarCategoria.bind(this)}
-          categorias={this.state.categorias}/>
+          adicionarCategoria={this.categorias.adicionarCategoria}
+          categorias={this.categorias.categorias}/>
 
           <ListaDeNotas 
-          apagarNota={this.deletarNota.bind(this)}
-          notas={this.state.notas}/>
+          apagarNota={this.notas.apagarNota}
+          notas={this.notas.notas}/>
         </main>
       </section>
     );
